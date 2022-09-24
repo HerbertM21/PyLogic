4c7937773e74d3dc83899d49e6bad2d77adfa068
HerbertM21 <herbertmm12@gmail.com>
2022-09-24 11:00:28 -0300

    Modificación sin importancia

--- 05. Algoritmos avanzados/Funciones recursivas.py
+++ 05. Algoritmos avanzados/Funciones recursivas.py
@@ -1,17 +1,17 @@
-def factorial_recursivo(n):
-    if n == 1:
-        return 1
-    else:
-        return n * factorial_recursivo(n-1)
+def factorial_recursivo(n): # 5! = 5 * 4 * 3 * 2 * 1
+    if n == 1: # Caso base
+        return 1 # 1! = 1
+    else: # Caso recursivo
+        return n * factorial_recursivo(n-1) # n! = n * (n-1)!
 
-def fibonacci_recursivo(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2)
+def fibonacci_recursivo(n): # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+    if n == 0: # Caso base
+        return 0 # 0
+    elif n == 1: # Caso base
+        return 1 # 1
+    else: # Caso recursivo
+        return fibonacci_recursivo(n-1) + fibonacci_recursivo(n-2) # n = n-1 + n-2
 
-n = int(input("Escriba el numero que quiere utilizar para la recursión: "))
-print("El factorial de", n ,"es:",factorial_recursivo(n))
-print("La serie fibonacci de", n ,"es:",fibonacci_recursivo(n))
+n = int(input("Escriba el numero que quiere utilizar para la recursión: ")) # 5
+print("El factorial de", n ,"es:",factorial_recursivo(n)) # 5! = 5 * 4 * 3 * 2 * 1
+print("La serie fibonacci de", n ,"es:",fibonacci_recursivo(n)) # 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...

b9fe36abdf61066d6dac5aa0db3e9a6e62c08e30
HerbertM21 <herbertmm12@gmail.com>
2022-10-06 23:32:01 -0300

    Version 6.0

---
+++ 03. Estructuras de control/Strings/Posicion _strings.py
@@ -0,0 +1,10 @@
+string = "Herbert Mayorga"
+
+# Devolver el string desde el inicio hasta el final
+print(string[:])
+# Devolver el string desde X letra del string.
+print(string[4:])
+# Devolver el string hasta X letra del string.
+print(string[:3])
+# Invertir string
+print(string[::-1])
